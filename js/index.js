505b9e5fda684b364312544f4f35bfabfc50b3bd
unknown <renanandreolla@outlook.com>
2019-07-31 16:30:41 -0300

    Primeiro envio

---
+++ js/index.js
@@ -0,0 +1,15 @@
+var age = parseInt(prompt("Digite sua idade:"));
+
+if(age >= 5 || <= 7) {
+  document.body.append("Sua categoria é Infantil A")
+}else if(age >= 8 || <= 10) {
+  document.body.append("Sua categoria é Infantil B")
+}else if(age >= 11 || <= 13) {
+  document.body.append("Sua categoria é Juvenil A")
+}else if(age >= 14 || <= 17) {
+  document.body.append("Sua categoria é Juvenil B")
+}else if(age >= 18) {
+  document.body.append("Sua categoria é Adulto")
+}else {
+  document.body.append("Não existe categoria para você")
+}}}}}}
