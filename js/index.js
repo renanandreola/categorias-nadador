6d23696c7e9d3362bb08b7a1bedffd9fc75523f8
Aluno <renanandreolla@outlook.com>
2019-07-31 19:18:32 -0300

    Alteração nos operadores, funcionando

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 var age = parseInt(prompt("Digite sua idade:"));
 
-if(age >= 5 || <= 7) {
+if(age >= 5 && age <= 7) {
   document.body.append("Sua categoria é Infantil A")
-}else if(age >= 8 || <= 10) {
+}else if(age >= 8 && age <= 10) {
   document.body.append("Sua categoria é Infantil B")
-}else if(age >= 11 || <= 13) {
+}else if(age >= 11 && age <= 13) {
   document.body.append("Sua categoria é Juvenil A")
-}else if(age >= 14 || <= 17) {
+}else if(age >= 14 && age <= 17) {
   document.body.append("Sua categoria é Juvenil B")
 }else if(age >= 18) {
   document.body.append("Sua categoria é Adulto")
 }else {
   document.body.append("Não existe categoria para você")
-}}}}}}
+}
